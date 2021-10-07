3534f2f46f434ed2c27690b96528644a48cc7838
Daniel Lafontant <ldanieljajr@gmail.com>
2021-10-07 13:33:00 -0400

    html

---
+++ index.js
@@ -0,0 +1,5 @@
+function sayhello(name) {
+    return "hello " + name;
+}
+var username = 'person';
+document.body.innerHTML = sayhello(username);
---
+++ index.ts
@@ -0,0 +1,6 @@
+function sayhello(name: string){
+    return "hello " + name;
+}
+
+var username = 'person'
+document.body.innerHTML = sayhello(username);
